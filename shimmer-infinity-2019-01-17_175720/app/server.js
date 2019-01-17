ca1ad74baf3391a972b762b6182f4ae700a92e7f
Hunny <devinhunny04021997@gmail.com>
2019-01-17 10:04:55 -0800

    Add files via upload

---
+++ shimmer-infinity-2019-01-17_175720/app/server.js
@@ -0,0 +1,30 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+app.get('/api/whoami', function(request, response) {
+  let ip = request.ip;
+  let language = request.acceptsLanguages();
+  let software = request.get('User-Agent');
+  response.json({ipaddress:ip,"language":language,"software":software});
+  
+  
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
